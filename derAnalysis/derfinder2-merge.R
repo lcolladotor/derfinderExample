a075d21007e5b809e04fb41b796da42a083f5637
lcolladotor <lcollado@jhsph.edu>
2013-08-15 21:56:29 -0400

    Updated the merging script for the new file configuration

--- derAnalysis/derfinder2-merge.R
+++ derAnalysis/derfinder2-merge.R
@@ -20,7 +20,7 @@ if (!is.null(opt$help)) {
 }
 
 ## Default value for prefix
-if (is.null(opt$prefix)) opt$prefix <- ""
+if (is.null(opt$prefix)) opt$prefix <- "run1"
 
 
 ## setup
@@ -33,34 +33,34 @@ for(chr in chrs) {
 	message(paste(Sys.time(), "Loading chromosome", chr))
 	
 	## Process the F-statistics
-	load(paste0(opt$prefix, "chr", chr, "/fstats-", chr, ".Rdata"))
+	load(paste0(opt$prefix, "/chr", chr, "/fstats-", chr, ".Rdata"))
 	fullFstats[[chr]] <- fstats
 	
 	## Process the regions, nullstats and nullwidths
-	load(paste0(opt$prefix, "chr", chr, "/regs-", chr, ".Rdata"))
+	load(paste0(opt$prefix, "/chr", chr, "/regs-", chr, ".Rdata"))
 	fullRegs[[chr]] <- regs$regions
 	fullNullstats[[chr]] <- regs$nullstats
 	fullNullwidths[[chr]] <- regs$nullwidths
 	
 	## Process the annotation results
-	load(paste0(opt$prefix, "chr", chr, "/annotation-", chr, ".Rdata"))
+	load(paste0(opt$prefix, "/chr", chr, "/annotation-", chr, ".Rdata"))
 	annotation$chr <- rep(chr, nrow(annotation))
 	fullAnno[[chr]] <- annotation
 	
 	## Process the timing information
-	load(paste0(opt$prefix, "chr", chr, "/timeinfo-", chr, ".Rdata"))
+	load(paste0(opt$prefix, "/chr", chr, "/timeinfo-", chr, ".Rdata"))
 	fullTime[[chr]] <- timeinfo
 }
 
 ## Save Fstats, Nullstats, and time info
 message(paste(Sys.time(), "Saving fullFstats"))
-save(fullFstats, file=paste0(opt$prefix, "fullFstats.Rdata"))
+save(fullFstats, file=paste0(opt$prefix, "/fullFstats.Rdata"))
 message(paste(Sys.time(), "Saving fullTime"))
-save(fullTime, file=paste0(opt$prefix, "fullTime.Rdata"))
+save(fullTime, file=paste0(opt$prefix, "/fullTime.Rdata"))
 message(paste(Sys.time(), "Saving fullNullstats"))
-save(fullNullstats, file=paste0(opt$prefix, "fullNullstats.Rdata"))
+save(fullNullstats, file=paste0(opt$prefix, "/fullNullstats.Rdata"))
 message(paste(Sys.time(), "Saving fullNullwidths"))
-save(fullNullwidths, file=paste0(opt$prefix, "fullNullwidths.Rdata"))
+save(fullNullwidths, file=paste0(opt$prefix, "/fullNullwidths.Rdata"))
 
 ## Save the annotation 
 fullAnnotation <- do.call(rbind, fullAnno)
@@ -74,9 +74,9 @@ identical(length(fullRegionsDF), dim(fullAnnotation)[1])
 
 ## save DF version
 message(paste(Sys.time(), "Saving fullAnnotation"))
-save(fullAnnotation, file=paste0(opt$prefix, "fullAnnotation.Rdata"))
+save(fullAnnotation, file=paste0(opt$prefix, "/fullAnnotation.Rdata"))
 message(paste(Sys.time(), "Saving fullRegionsDF"))
-save(fullRegionsDF, file=paste0(opt$prefix, "fullRegionsDF.Rdata"))
+save(fullRegionsDF, file=paste0(opt$prefix, "/fullRegionsDF.Rdata"))
 
 ## Transform fullRegionsDF to a regular data.frame
 names(fullRegionsDF) <- seq_len(length(fullRegionsDF))
@@ -89,7 +89,7 @@ fullRegions <- fullRegions[, c("chr", "start", "end", "value", "area", "cluster"
 
 ## save data.frame version
 message(paste(Sys.time(), "Saving fullRegions"))
-save(fullRegions, file=paste0(opt$prefix, "fullRegions.Rdata"))
+save(fullRegions, file=paste0(opt$prefix, "/fullRegions.Rdata"))
 
 
 ## save a huge data frame with the information merged
@@ -97,7 +97,7 @@ fullRegsAnno <- cbind(fullRegions, fullAnnotation)
 colnames(fullRegsAnno)[ncol(fullRegsAnno)] <- "chrnum"
 
 message(paste(Sys.time(), "Saving fullRegsAnno"))
-save(fullRegsAnno, file=paste0(opt$prefix, "fullRegsAnno.Rdata"))
+save(fullRegsAnno, file=paste0(opt$prefix, "/fullRegsAnno.Rdata"))
 
 print(proc.time())
 sessionInfo()
