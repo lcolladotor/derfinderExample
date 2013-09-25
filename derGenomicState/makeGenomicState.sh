431fd47fcf41434f10087edae8118969ebc062ba
lcolladotor <lcollado@jhsph.edu>
2013-09-25 16:10:51 -0400

    Made it a bit more polished

--- derGenomicState/makeGenomicState.sh
+++ derGenomicState/makeGenomicState.sh
@@ -8,7 +8,11 @@
 echo "**** Job starts ****"
 date
 
+mkdir -p logs
+
 R -e "library(derfinder2); library('TxDb.Hsapiens.UCSC.hg19.knownGene'); txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene; GenomicState.Hsapiens.UCSC.hg19.knownGene <- makeGenomicState(txdb=txdb); save(GenomicState.Hsapiens.UCSC.hg19.knownGene, file='GenomicState.Hsapiens.UCSC.hg19.knownGene.Rdata'); proc.time(); sessionInfo()"
+
+mv genState.* logs/
 
 ### Done
 echo "**** Job ends ****"
