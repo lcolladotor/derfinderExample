e6e8f615724dcd04a892afe87a7c0726908ba607
lcolladotor <lcollado@jhsph.edu>
2013-09-25 00:38:31 -0400

    Changed to using fullCoverage(). Helps by reading the raw data only once.

--- derCoverageInfo/README.md
+++ derCoverageInfo/README.md
@@ -1,16 +1,4 @@
 Explanation of files
 ====================
 
-This folder has the scripts for extracting the coverage information from the BAM files and filtering it before the main steps in the __derfinder2__ analysis pipeline.
-
-# derCoverageInfo.sh
-
-This shell script runs __derfinder2__ data processing steps: __makeBamList()__ and __loadCoverage()__. The coverage cutoff used is set to 5 and the output files will later be used in `../derAnalysis`.
-
-It creates several hidden shell scripts that are then submitted to the cluster.
-
-# mergeCov.R
-
-This R script merges the output from `derCoverageInfo.sh` into `covList.Rdata`. This file might be useful for other downstream purposes like plotting.
-
-The shell script `runMerge.sh` is just a helper script.
+This folder contains the filtered coverage information obtained from [fullCoverage](https://github.com/lcolladotor/derfinderExample/tree/master/fullCoverage) which is then used by the  __derfinder2__ analysis pipeline.
--- fullCoverage/fullCoverage.sh
+++ fullCoverage/fullCoverage.sh
@@ -6,14 +6,12 @@ WDIR=${MAINDIR}/fullCoverage
 DATADIR=${MAINDIR}/thout
 
 # Define variables
-SHORT='derEx-fullCov'
+SHORT='fullCov-derEx'
+CORES=24
 
-# Construct shell files
-for chrnum in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y
-do
-	chr="chr${chrnum}"
-	echo "Creating script for chromosome ${chr}"
-	cat > ${WDIR}/.${SHORT}.${chr}.sh <<EOF
+# Construct shell file
+echo "Creating script for loading the Coverage data"
+cat > ${WDIR}/.${SHORT}.sh <<EOF
 #!/bin/bash	
 echo "**** Job starts ****"
 date
@@ -21,16 +19,17 @@ date
 # Make logs directory
 mkdir -p ${WDIR}/logs
 
-# run makeDF()
-R-devel -e "library(derfinder2); dirs <- makeBamList(datadir='${DATADIR}', samplepatt='RR'); loadCoverage(dirs=dirs, chr='${chrnum}', cutoff=NULL, output='auto', verbose=TRUE); proc.time(); sessionInfo()"
+# Load the data, save the coverage without filtering, then save each file separately
+R-devel -e "library(derfinder2); dirs <- makeBamList(datadir='${DATADIR}', samplepatt='RR'); cores <- ${CORES}; source('fullCoverage.R')"
 
 ## Move log files into the logs directory
-mv ${SHORT}.${chr}.* ${WDIR}/logs/
+mv ${SHORT}.* ${WDIR}/logs/
 
 echo "**** Job ends ****"
 date
 EOF
-	call="qsub -cwd -l jabba,mem_free=3G,h_vmem=10G,h_fsize=10G -N ${SHORT}.${chr} -m e ${WDIR}/.${SHORT}.${chr}.sh"
-	echo $call
-	$call
-done
+
+## Memory limits set assuming 24 cores will be used
+call="qsub -cwd -l jabba,mem_free=72G,h_vmem=10G,h_fsize=10G -pe local ${CORES} -N ${SHORT} -m e ${WDIR}/.${SHORT}.sh"
+echo $call
+$call
